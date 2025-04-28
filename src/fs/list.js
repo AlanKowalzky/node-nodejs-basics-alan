b1b6ba3df08eb897929013c925ca48bf3775a14f
AlanKowalzky <alan.kowalzky@gmail.com>
2025-04-28 13:44:07 +0200

    refactor: finish modules on task-1

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,21 +1,5 @@
-import { access, constants, readdir } from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const list = async () => {
-    const folderPath = path.join(__dirname, 'files');
-    
-    try {
-        await access(folderPath, constants.F_OK);
-    } catch (error) {
-        throw new Error('FS operation failed');
-    }
-    
-    const files = await readdir(folderPath);
-    console.log(files);
+    // Write your code here 
 };
 
 await list();
