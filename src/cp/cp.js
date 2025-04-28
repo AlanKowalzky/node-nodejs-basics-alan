8cfa1716032d58302dd8947dc8a493d2cd841855
AlanKowalzky <alan.kowalzky@gmail.com>
2025-04-28 23:59:40 +0200

    feat: finish cp child  on task-1

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { spawn } from 'node:child_process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = path.join(__dirname, 'files', 'script.js');
+    const child = spawn('node', [scriptPath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+        process.stdin.pipe(child.stdin);
+        child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+
+spawnChildProcess(['foo', 'bar']);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,10 +1,6 @@
 import { Worker } from 'node:worker_threads';
 import { cpus } from 'node:os';
 import path from 'node:path';
-// import { fileURLToPath } from 'node:url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const performCalculations = async () => {
 	const numCPUs = cpus().length;
@@ -16,7 +12,7 @@ const performCalculations = async () => {
 		results[i] = new Promise((resolve) => {
 			const worker = new Worker(workerFile, {
 				workerData: 10 + i,
-				// type: 'module',
+
 			});
 			workers.push(worker);
 
