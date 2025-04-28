5d385e18d388fbecf0d2a699e68f3538dca86046
AlanKowalzky <alan.kowalzky@gmail.com>
2025-04-28 19:51:06 +0200

    feat: finish zip on task-1

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -3,14 +3,13 @@ import process from 'node:process';
 import { pipeline } from 'node:stream/promises';
 
 const transform = async () => {
-    const upperCaseTransform = new Transform({
+    const reverseTransform = new Transform({
         transform(chunk, encoding, callback) {
-            // const upperChunk = chunk.toString().toUpperCase();
-            // callback(null, upperChunk);
+                   
             callback(null, chunk.toString().split('').reverse().join(''));
         }
     });
 
-    await pipeline(process.stdin, upperCaseTransform, process.stdout);
+    await pipeline(process.stdin, reverseTransform, process.stdout);
 }
 await transform();
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import zlib from 'node:zlib';
+
+
+
 const compress = async () => {
-    // Write your code here 
+    const sourceFilePath = path.resolve(import.meta.dirname, 'files', 'fileToCompress.txt');
+    const destinationFilePath = path.resolve(import.meta.dirname, 'files', 'archive.gz');
+
+    const sourceStream = fs.createReadStream(sourceFilePath);
+    const destinationStream = fs.createWriteStream(destinationFilePath);
+    const gzip = zlib.createGzip();
+
+    sourceStream.pipe(gzip).pipe(destinationStream);
+
+    
+
+
 };
 
 await compress();
