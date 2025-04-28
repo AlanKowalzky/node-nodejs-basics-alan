2bce3b7f166c8040d6d593b8ccb6de9e93b69401
AlanKowalzky <alan.kowalzky@gmail.com>
2025-04-28 15:22:06 +0200

    feat: finish hash on task-1

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+import path from 'node:path';
+
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    
+    const filePath = path.resolve(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+    const fileStream = fs.createReadStream(filePath);
+    const hash = crypto.createHash('sha256');
+
+    
+    fileStream
+        .on('data', (chunk) => {
+            hash.update(chunk);
+    })
+       .on('end', () => {
+            const result = hash.digest('hex');
+            console.log('SHA-256 Hash:', result);
+    })
+        .on('error', (err) => {
+        console.error('Error reading file:', err);
+    });
 };
+
+
+
 
 await calculateHash();
