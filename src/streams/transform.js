200dfa9bf131c515e30d0ee1f0098470a211ff7a
AlanKowalzky <alan.kowalzky@gmail.com>
2025-04-28 17:45:36 +0200

    feat: finish streams on task-1

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { Transform } from 'node:stream';
+import process from 'node:process';
+import { pipeline } from 'node:stream/promises';
 
+const transform = async () => {
+    const upperCaseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            // const upperChunk = chunk.toString().toUpperCase();
+            // callback(null, upperChunk);
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    });
+
+    await pipeline(process.stdin, upperCaseTransform, process.stdout);
+}
 await transform();
