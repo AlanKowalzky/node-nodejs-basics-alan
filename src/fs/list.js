d2b8a8ce128f2854a65552e9d7c1f50acc6a9f2a
AlanKowalzky <alan.kowalzky@gmail.com>
2025-04-28 21:51:34 +0200

    fix: finish workers and l on task-1

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { access, constants, readdir } from 'fs/promises';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join(import.meta.dirname, 'files');
+    try {
+        await access(folderPath, constants.F_OK);
+    } catch {
+        throw new Error('FS operation failed');
+    }
+    const files = await readdir(folderPath);
+    console.log(files);
 };
 
 await list();
