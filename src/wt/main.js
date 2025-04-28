5ef3274c07dac2049f5a80eb5060b3354d58d697
AlanKowalzky <alan.kowalzky@gmail.com>
2025-04-28 21:36:17 +0200

    feat: finish workers on task-1

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+	const numCPUs = cpus().length;
+	const results = [];
+	const workers = [];
+	const workerFile = path.join(import.meta.dirname, 'worker.js');
+
+	for (let i = 0; i < numCPUs; i++) {
+		results[i] = new Promise((resolve) => {
+			const worker = new Worker(workerFile, {
+				workerData: 10 + i,
+				// type: 'module',
+			});
+			workers.push(worker);
+
+			worker.on('message', (msg) => {
+				resolve({ status: 'resolved', data: msg });
+			});
+			worker.on('error', () => {
+				resolve({ status: 'error', data: null });
+			});
+		});
+	}
+
+	const finalResults = await Promise.all(results);
+	console.log(finalResults);
+
+	workers.forEach((worker) => worker.terminate());
 };
 
 await performCalculations();
